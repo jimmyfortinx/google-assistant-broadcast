2ceaf8e5c22816e860f06bfc30662a010329c5b4
Jimmy Fortin <jimmy.fortin@gmail.com>
2022-10-16 20:38:19 -0400

    remove broadcast prefix

--- assistant.js
+++ assistant.js
@@ -6,12 +6,12 @@ const fs = require("fs");
 /** See https://github.com/endoplasmic/google-assistant for more info **/
 const config = {
   auth: {
-    keyFilePath: process.env.CLIENT_SECRET
-        || "/usr/src/config/client_secret.json",
-    savedTokensPath: process.env.TOKEN || "/usr/src/config/tokens.json"
+    keyFilePath:
+      process.env.CLIENT_SECRET || "/usr/src/config/client_secret.json",
+    savedTokensPath: process.env.TOKEN || "/usr/src/config/tokens.json",
   },
   conversation: {
-    lang: process.env.LANGUAGE || 'en-GB', // defaults to en-GB, but try other ones, it's fun!
+    lang: process.env.LANGUAGE || "en-GB", // defaults to en-GB, but try other ones, it's fun!
     isNew: true,
   },
 };
@@ -30,81 +30,87 @@ const checkFileExistsSync = (filepath) => {
 
 const readJsonFile = (filepath) => {
   try {
-    return JSON.parse(fs.readFileSync(filepath, 'utf8'));
+    return JSON.parse(fs.readFileSync(filepath, "utf8"));
   } catch (e) {
     console.log(e);
-    exitAndLogError(`Failed reading JSON file at path ${filepath}`)
+    exitAndLogError(`Failed reading JSON file at path ${filepath}`);
   }
-}
+};
 
 const exitAndLogError = (errorMsg) => {
-  console.log(`[ERROR] ${errorMsg}`)
-  process.exit(1)
-}
+  console.log(`[ERROR] ${errorMsg}`);
+  process.exit(1);
+};
 
 function Assistant() {
   this.validateFiles = () => {
     if (!checkFileExistsSync(config.auth.keyFilePath)) {
       exitAndLogError(
-          `Client Secret file at path '${config.auth.keyFilePath}' does not exist.`)
+        `Client Secret file at path '${config.auth.keyFilePath}' does not exist.`
+      );
     }
 
     if (!checkFileExistsSync(config.auth.savedTokensPath)) {
       exitAndLogError(
-          `Tokens file at path '${config.auth.savedTokensPath}' does not exist.`)
+        `Tokens file at path '${config.auth.savedTokensPath}' does not exist.`
+      );
     }
 
     const secretFileContent = readJsonFile(config.auth.keyFilePath).installed;
 
     if (secretFileContent.token_uri !== "https://oauth2.googleapis.com/token") {
       exitAndLogError(
-          `The Client Secret file at path '${config.auth.keyFilePath}' has invalid 'token_uri' value. Expecting value 'https://oauth2.googleapis.com/token', but was '${secretFileContent.token_uri}'. Please make sure you download OAuth client file from GCP Console / API & Services / Credentials.`)
+        `The Client Secret file at path '${config.auth.keyFilePath}' has invalid 'token_uri' value. Expecting value 'https://oauth2.googleapis.com/token', but was '${secretFileContent.token_uri}'. Please make sure you download OAuth client file from GCP Console / API & Services / Credentials.`
+      );
     }
 
     if (!secretFileContent.redirect_uris) {
       exitAndLogError(
-          `The Client Secret file at path '${config.auth.keyFilePath}' is missing 'redirect_uris' property. Please make sure you download OAuth client file from GCP Console / API & Services / Credentials.`)
+        `The Client Secret file at path '${config.auth.keyFilePath}' is missing 'redirect_uris' property. Please make sure you download OAuth client file from GCP Console / API & Services / Credentials.`
+      );
     }
-  }
+  };
 
   this.cast = async (message) => {
     const assistant = new GoogleAssistant(config.auth);
-    config.conversation.textQuery = `Broadcast ${message}`;
+    config.conversation.textQuery = message;
 
-    console.log(`Sending message (${config.conversation.lang}):`,
-        config.conversation.textQuery);
+    console.log(
+      `Sending message (${config.conversation.lang}):`,
+      config.conversation.textQuery
+    );
 
     return new Promise((resolve, reject) => {
       assistant
-      .on("ready", () => assistant.start(config.conversation))
-      .on("started", (conversation) => {
-        conversation
-        .on("response", (text) => {
-          const response = text || "empty"
-          console.log("[OK] Conversation Response: ", response);
-          resolve(response);
-        })
-        .on('ended', (error, continueConversation) => {
-          if (error) {
-            console.log('[ERROR] Conversation Ended Error:', error);
-          } else if (continueConversation) {
-            console.log('[WARN] Conversation continue is not handled');
-          } else {
-            console.log('[OK] Conversation Completed');
-            conversation.end();
-          }
+        .on("ready", () => assistant.start(config.conversation))
+        .on("started", (conversation) => {
+          conversation
+            .on("response", (text) => {
+              const response = text || "empty";
+              console.log("[OK] Conversation Response: ", response);
+              resolve(response);
+            })
+            .on("ended", (error, continueConversation) => {
+              if (error) {
+                console.log("[ERROR] Conversation Ended Error:", error);
+              } else if (continueConversation) {
+                console.log("[WARN] Conversation continue is not handled");
+              } else {
+                console.log("[OK] Conversation Completed");
+                conversation.end();
+              }
+            })
+            .on("error", (error) => {
+              console.log("[ERROR] Error while broadcasting:", error);
+              reject(new Error(`Error while broadcasting: ${error}`));
+            });
         })
         .on("error", (error) => {
-          console.log("[ERROR] Error while broadcasting:", error);
+          console.log("[ERROR] Error while broadcasting: ", error);
           reject(new Error(`Error while broadcasting: ${error}`));
-        })
-      })
-      .on("error", (error) => {
-        console.log("[ERROR] Error while broadcasting: ", error);
-        reject(new Error(`Error while broadcasting: ${error}`));
-      });
-    })
-  }
+        });
+    });
+  };
 }
 
 module.exports = Assistant;
